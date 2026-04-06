acd40bcdbeb8c0d18cfe46f6e597e037e2ad8a80
James Mascarenhas <james.mas27@gmail.com>
2026-04-06 14:07:47 -0400

    add onehot_wrapper as it's own file

---
+++ PPO/onehot_wrapper.py
@@ -0,0 +1,20 @@
+import gymnasium as gym
+import numpy as np
+from gymnasium import spaces
+
+
+class OneHotWrapper(gym.ObservationWrapper):
+    def __init__(self, env):
+        super().__init__(env)
+        self.n_states = env.observation_space.n
+        self.observation_space = spaces.Box(
+            low = 0.0,
+            high = 1.0,
+            shape = (self.n_states,),
+            dtype = np.float32,
+        )
+
+    def observation(self, obs):
+        one_hot = np.zeros(self.n_states, dtype=np.float32)
+        one_hot[int(obs)] = 1.0
+        return one_hot
